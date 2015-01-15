414037d38ca1c58f35e07e3be1ecda1ce2b45cfc
Mathy <vanhoefm@users.noreply.github.com>
2015-01-15 16:59:42 -0500

    ath9k_htc: add missing switch case to wmi_cmd_to_name

--- drivers/net/wireless/ath/ath9k/wmi.c
+++ drivers/net/wireless/ath/ath9k/wmi.c
@@ -87,6 +87,8 @@ static const char *wmi_cmd_to_name(enum wmi_cmd_id wmi_cmd)
 		return "WMI_REACTIVEJAM_CMDID";
 	case WMI_FASTREPLY_CMDID:
 		return "WMI_FASTREPLY_CMDID";
+	case WMI_CONSTANTJAM_CMDID:
+		return "WMI_CONSTANTJAM_CMDID";
 	}
 
 	return "Bogus";
